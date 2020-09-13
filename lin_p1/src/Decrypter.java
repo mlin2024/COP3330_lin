eee1ce595ff767e969e11239be41216198c9399a
Meghan Lin <mlin2024@gmail.com>
2020-09-13 16:46:03 -0400

    Created encrypter/decrypter classes and test classes

--- lin_p1/src/Decrypter.java
+++ lin_p1/src/Decrypter.java
@@ -1,4 +1,4 @@
-public class First {
+public class Decrypter {
     public static void main(String[] args) {
 
     }
--- lin_p1/src/Encrypter.java
+++ lin_p1/src/Encrypter.java
@@ -1,4 +1,4 @@
-public class First {
+public class Encrypter {
     public static void main(String[] args) {
 
     }
