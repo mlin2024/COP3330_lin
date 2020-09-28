3b62f5064956e775da858acefc36ceac2723cc26
Meghan Lin <mlin2024@gmail.com>
2020-09-28 17:43:47 -0400

    Created test cases for the BodyMassIndex constructor and for each BMI category

--- lin_p2/src/BodyMassIndexTest.java
+++ lin_p2/src/BodyMassIndexTest.java
@@ -1,5 +1,33 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BodyMassIndexTest {
+    @Test
+    void testConstructor() {
+        BodyMassIndex bmi = new BodyMassIndex(61, 105);
+        assertEquals(bmi.height, 61);
+        assertEquals(bmi.weight, 105);
+        assertEquals(bmi.bmi, 19.8374093);
+    }
 
+    void testUnderweight() {
+        BodyMassIndex bmi = new BodyMassIndex(61, 10);
+        assertEquals(bmi.category, "Underweight");
+    }
+
+    void testNormalWeight() {
+        BodyMassIndex bmi = new BodyMassIndex(61, 105);
+        assertEquals(bmi.category, "Normal weight");
+    }
+
+    void testOverweight() {
+        BodyMassIndex bmi = new BodyMassIndex(61, 150);
+        assertEquals(bmi.category, "Overweight");
+    }
+
+    void testObese() {
+        BodyMassIndex bmi = new BodyMassIndex(61, 1000);
+        assertEquals(bmi.category, "Obese");
+    }
 }
