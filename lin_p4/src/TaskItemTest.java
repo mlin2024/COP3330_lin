ede6accdc08a60a4ab1993ca7c99842bbbb860f2
Meghan Lin <mlin2024@gmail.com>
2020-11-16 13:50:00 -0500

    Added blank test functions

--- lin_p4/src/TaskItemTest.java
+++ lin_p4/src/TaskItemTest.java
@@ -1,5 +1,45 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaskItemTest {
+    @Test
+    public void creatingTaskItemFailsWithInvalidDueDate() {
 
+    }
+
+    @Test
+    public void creatingTaskItemFailsWithInvalidTitle() {
+
+    }
+
+    @Test
+    public void creatingTaskItemSucceedsWithValidDueDate() {
+
+    }
+
+    @Test
+    public void creatingTaskItemSucceedsWithValidTitle() {
+
+    }
+
+    @Test
+    public void settingTaskItemDueDateFailsWithInvalidDate() {
+
+    }
+
+    @Test
+    public void settingTaskItemDueDateSucceedsWithValidDate() {
+
+    }
+
+    @Test
+    public void settingTaskItemTitleFailsWithInvalidTitle() {
+
+    }
+
+    @Test
+    public void settingTaskItemTitleSucceedsWithValidTitle() {
+
+    }
 }
